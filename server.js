2e3a70dba47f2e68cd659fbcdae8356f361cf2e2
Nacho <ignagus4432@gmail.com>
2024-08-14 20:23:46 -0300

    Proyecto organizado

--- server.js
+++ server.js
@@ -1,87 +1,12 @@
 import express from 'express';
+import alumnoRoutes from "./src/alumno/alumno.routes.js"
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-let alumnos = [];
-let idActual = 1;
-
-app.post('/alumno',(req,res) =>{
-    const nuevoAlumno = {
-        id: idActual++,
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        curso: req.body.curso,
-    };
-
-    alumnos.push(nuevoAlumno);
-    res.status(201).json({
-        msg: `Se creo correctamen el alumno con id ${nuevoAlumno.id} nombre ${nuevoAlumno.nombre}`,
-    });
-});
-
-app.get('/alumno', (req,res) =>{
-    res.json(alumnos);
-});
-
-app.get('/alumno/:id',(req,res) =>{
-    try{
-        const id = parseInt(req.params.id);
-    if(isNaN(id)) {
-        throw new Error('El id no es un entero');
-    }
-    const alumno = alumnos.find((a) => a.id === id);
-
-    if(!alumno) throw new Error('Alumno no encontrado');
-
-    res.json(alumno);
-
-    }catch(error){
-        res.status(404).json(error.message);
-    }
-});
-
-app.put('/alumno/:id', (req,res) =>{
-    try{
-        const id = parseInt(req.params.id);
-
-    if (isNaN(id)) {
-        throw new Error('El id no es un entero');
-    }
-    const alumno = alumnos.find((a) => a.id === id);
-
-    if(!alumno) throw new Error('Alumno no encontrado');
-
-    alumno.nombre = req.body.nombre ?? alumno.nombre;
-    alumno.apellido = req.body.apellido ?? alumno.apellido;
-    alumno.curso = req.body.curso ?? alumno.curso;
-
-    res.json(alumno);
-
-    }catch(error){
-        res.status(404).json(error.message);
-    }
-});
-
-app.delete('/alumno/:id', (req,res) => {
-    try{
-        const id = parseInt(req.params.id);
-
-    if (isNaN(id)) {
-        throw new Error('El id no es un entero');
-    }
-    const index = alumnos.find((a) => a.id === id);
-
-    const borrarAlumno = alumnos.splice(index, 1);
-
-    res.json(borrarAlumno);
-
-    }catch(error){
-        res.status(404).json(error.message);
-    }
-});
+app.use('/api', alumnoRoutes);
 
 app.listen(port, () => {
     console.log(`App lista en el puerto ${port}`);
